5810b97c8732eb36b1b36755d4d55f29775b37ce
oscarraichert <oscarraichert@hotmail.com>
2022-07-03 06:44:05 -0300

    mudancaaaaaas

--- src/injected/objectives/arena/atacarPlayer.ts
+++ src/injected/objectives/arena/atacarPlayer.ts
@@ -1,4 +1,5 @@
 import { clickAndWait, getByXpath, getFromStorage, log, logError, timeout } from "../../../services/background_utils";
+import { Log, TipoLog } from "../../../services/model/log";
 
 (async () => {
     try {
@@ -13,7 +14,7 @@ async function setPlayerName() {
     let inputNome = getByXpath('//*[@id="ujn"]') as HTMLInputElement;
     let player = await getFromStorage("currentAtkPlayer") as string;
     inputNome.value = player;
-    await log({ type: "info", message: `Inserindo nome para ser atacado: ${player}` });
+    await log(new Log(TipoLog.info, `Inserindo nome para ser atacado: ${player}`));
 }
 
 async function tryUntil() {
--- src/injected/objectives/vistaGeral/selecionarEquipamento.ts
+++ src/injected/objectives/vistaGeral/selecionarEquipamento.ts
@@ -1,9 +1,10 @@
 import { clickAndWait, log, logError } from "../../../services/background_utils";
+import { Log, TipoLog } from "../../../services/model/log";
 
 (async () => {
     try {
         await clickAndWait('//*[@id="char"]/div[10]/div', 1000);
-        await log({type: "info", message: 'Selecionando equipamento'});
+        await log(new Log(TipoLog.info, "Selecionando equipamento"));
 
     } catch (e) {
         await logError(e);
