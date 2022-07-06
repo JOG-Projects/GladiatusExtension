719913cd5ed4726db283c9c39d6d33c2cb6412e2
João Xavier <fastjonh@gmail.com>
2022-07-06 18:09:31 -0300

    alterando nome do script

--- src/injected/bensGerais/obterQtdComidaInv.ts
+++ src/injected/bensGerais/obterQtdComidaInv.ts
@@ -2,14 +2,14 @@ import { doWork, log, setStorage, tryUntil } from "../../services/utils";
 import { TipoLog } from "../../services/model/tipoLog";
 
 
-doWork("obterQtdComida", async () => {
+doWork("obterQtdComidaInv", async () => {
     await tryUntil(async () => getQntComida());
 });
 
 async function getQntComida(): Promise<void> {
     let elemento = document.getElementById('inv') as HTMLElement;
     let qtdComida = elemento.children.length;
-    await log(TipoLog.info, `Quantidade comida ${qtdComida}`)
+    await log(TipoLog.info, `Quantidade comida no inventario: ${qtdComida}`)
 
-    await setStorage('qtdComida', qtdComida);
+    await setStorage('qtdComidaInv', qtdComida);
 }
--- src/services/controllers/comprarComida.ts
+++ src/services/controllers/comprarComida.ts
@@ -3,8 +3,8 @@ import { execute, getFromStorage } from "../utils";
 export async function comprarComida(): Promise<void>{
     await execute('abrirBensGerais');
     await execute('abrirTabComidas');
-    await execute('obterQtdComida');
-    let qtdComida = await getFromStorage<number>('qtdComida');
+    await execute('obterQtdComidaInv');
+    let qtdComida = await getFromStorage<number>('qtdComidaInv');
     let qtdComidaMin = await getFromStorage<number>('qtdComidaMin');
 
     if (qtdComida > qtdComidaMin){
