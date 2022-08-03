3e7ce70e8c3d714a2aa898e29546b4a3a9ed888e
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2022-08-03 00:18:33 -0300

    fix da arena

--- src/features/arena/injected/getArenaCooldown.ts
+++ src/features/arena/injected/getArenaCooldown.ts
@@ -2,4 +2,4 @@ import { getAtkCooldown } from "../../ataque_utils";
 import { doWork, tryUntil } from "../../utils";
 
 
-doWork("getArenaCooldown", async () => await getAtkCooldown('//*[@id="cooldown_bar_text_arena"]', "arenaCooldown"));
+doWork(async () => await getAtkCooldown('//*[@id="cooldown_bar_text_arena"]', "arenaCooldown"));
